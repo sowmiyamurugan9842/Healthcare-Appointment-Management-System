9cdf11893ce8cfe621280c2950f6f52544ccf4b3
Sowmiya_Murugan <sowmiyamurugan9842@gmail.com>
2026-07-12 12:18:54 +0530

    Add Healthcare Appointment Management System project

---
+++ src/main/java/com/example/healthcareappointmentmanagementsystem/HealthcareAppointmentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthcareappointmentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareAppointmentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareAppointmentManagementSystemApplication.class, args);
+	}
+
+}
